5dcbe5ca6975f86bc740b2fbaea63d1cb0baaaaf
Brian Perry <contact.brianperry@gmail.com>
2020-11-26 09:35:21 -0600

    Add controls to results-tracker story

--- buildless/stories/ResultsTracker.stories.js
+++ buildless/stories/ResultsTracker.stories.js
@@ -1,22 +1,33 @@
 import { html } from "lit-html"
 import "../index.js"
 
+const ResultsTrackerTemplate = ({ headline, race, total }) => {
+  return html`
+    <results-tracker
+      headline=${headline}
+      race=${race}
+      total=${total}
+      threshold="270"
+      primary="electoral"
+      secondary="popular"
+      candidates='[
+        {"name": "Joseph R. Biden Jr.", "primary": 253, "secondary": 73879622},
+        {"name": "Donald J. Trump", "primary": 214, "secondary": 69772905}
+      ]'
+    />
+  `;
+}
+
 export default {
   title: 'Components/Results Tracker',
   component: "results-tracker",
 }
 
-export const Primary = () => html`
-  <results-tracker 
-    headline="Race Between Old Men Too Close To Call"
-    race="President"
-    total="538"
-    threshold="270"
-    primary="electoral"
-    secondary="popular"
-    candidates='[
-        {"name": "Joseph R. Biden Jr.", "primary": 253, "secondary": 73879622},
-        {"name": "Donald J. Trump", "primary": 214, "secondary": 69772905}
-      ]' 
-    />
-  `;
+const Template = (args) => ResultsTrackerTemplate(args);
+
+export const Primary = Template.bind({});
+Primary.args = {
+  headline: "Race Between Old Men Too Close To Call",
+  race: "President",
+  total: "538",
+}
