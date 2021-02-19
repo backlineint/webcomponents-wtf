62e4fa3b7e477287c5822515f2a09ef08180190b
Brian Perry <contact.brianperry@gmail.com>
2021-02-19 11:33:35 -0600

    Formatting updates

--- everywhere/with-react/src/SwitchProvider.js
+++ everywhere/with-react/src/SwitchProvider.js
@@ -1,19 +1,23 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef } from "react";
 
-import '@generic-components/components/switch.js';
+import "@generic-components/components/switch.js";
 
 const SwitchProvider = ({ checked, handleToggle, children }) => {
-    const switchRef = useRef();
+  const switchRef = useRef();
 
-    useEffect(() => {
-        switchRef.current.addEventListener('checked-changed', e => {
-            handleToggle(switchRef.current.__checked);
-        });
-    }, []) // eslint-disable-line react-hooks/exhaustive-deps
-    // Can't add handleToggle as a dependency above because it would result in
-    // this hook being caled multiple times
+  useEffect(() => {
+    switchRef.current.addEventListener("checked-changed", (e) => {
+      handleToggle(switchRef.current.__checked);
+    });
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  // Can't add handleToggle as a dependency above because it would result in
+  // this hook being called multiple times
 
-    return <generic-switch checked={checked} ref={switchRef}>{children}</generic-switch>
-}
+  return (
+    <generic-switch checked={checked} ref={switchRef}>
+      {children}
+    </generic-switch>
+  );
+};
 
 export default SwitchProvider;
